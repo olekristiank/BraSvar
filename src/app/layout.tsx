2789063dd496d7d9aba24f84d4e5c0cfed02ccb1
Ole Kristian Klevstrand <ok@klevstrand.no>
2026-03-19 22:32:28 +0100

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Outfit, Manrope } from "next/font/google";
+import { Outfit, Manrope, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -27,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${outfit.variable} ${manrope.variable}`}>
+    <html lang="en" className={cn(outfit.variable, manrope.variable, "font-sans", geist.variable)}>
       <body>{children}</body>
     </html>
   );
